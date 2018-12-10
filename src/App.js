90fc4a96bea22f9aadc273db871a0468eaee95e8
Maya Davis <kryvitskaya@gmail.com>
2018-12-10 16:04:54 -0500

    categories separation

--- src/App.js
+++ src/App.js
@@ -12,23 +12,7 @@ class App extends React.Component {
   
   render() {
     return (
-      <Router>
-        <div className="App">
-
-          <div className="container">
-            <ul>
-              <Link to="/categories">Categories</Link>
-            </ul>
-            <hr/>
-
-          
-            <Route path="/categories" component={Categories} />
-         
-
-
-          </div>
-        </div>
-      </Router>
+      <Categories />
     );
   }
 }
--- src/CategoriesComponent.js
+++ src/CategoriesComponent.js
@@ -7,6 +7,7 @@ import {
     Redirect,
     HashRouter
   } from 'react-router-dom';
+  import categories from './lib/categories';
   
 class Categories extends React.Component {
   constructor(props) {
@@ -14,60 +15,45 @@ class Categories extends React.Component {
     this.state = {
       categories: [] 
     }
-  }
-  getCategories() {
-    fetch('http://apps.wearetouro.com/api/torahSubcategories/catsSubcats%7CtouroMain')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      //console.log(data);
-      let categoriesRepeated = [];
-      data.forEach(function(item){
-        // console.log(item.categoryValue);
-        categoriesRepeated.push(item.categoryValue);
-        
-      });
-      var categories = categoriesRepeated.filter(function(item, index){
-        return categoriesRepeated.indexOf(item) >= index;
-      });
-      this.setState({
-        categories
-      });
-      console.log('categories', categories);
-    });
+    console.log(this.state)
   }
   
   componentDidMount() {
-    this.getCategories();
+    categories().then(categories => {
+      console.log('categories', categories);
+      this.setState = {
+        categories 
+      }
+      console.log('state', this.state);
+    });
   }
   render () {
-    const {match, location} = this.props;
-    console.log(match);
     return (
-      <div>
-          <div className="jumbotron">
-              <h1 className="display-3">My Books</h1>
-          </div>
-        <div className="container">
-          <div className="row">
-              <div className="col-md-3">
-        <ul>
-        {
-           this.state.categories.map(
-            (category, i) => {
-            return <li><Link to={`${match.url}/${category}`}>{category}</Link></li>;
-           })
-        }
-        </ul>
-              </div>
-              <div className="col-md-9">
-                  {/* <Route path={`${match.path}/react`} render={() => { return <h1>React by Fullstack.io book</h1> }}/> */}
-                  <Route path={`${match.path}/:id`} component={Child} />
-              </div>
-          </div>
-          </div>
-      </div>
+      <Router>
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-3">Categories</h1>
+            </div>
+          <div className="container">
+            <div className="row">
+                <div className="col-md-3">
+          <ul>
+          {
+            this.state.categories.map(
+              (category, i) => {
+                return <li><Link to={`/${category}`}>{category}</Link></li>;
+            })
+          }
+          </ul>
+                </div>
+                <div className="col-md-9">
+                    {/* <Route path={`${match.path}/react`} render={() => { return <h1>React by Fullstack.io book</h1> }}/> */}
+                    <Route path={`/:id`} component={Child} />
+                </div>
+            </div>
+            </div>
+        </div>
+      </Router>   
   );
   }
     
