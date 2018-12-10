62bb06f78683d1440cf950b26b567a92f1c34c69
Maya Davis <kryvitskaya@gmail.com>
2018-12-10 16:07:17 -0500

    lib

---
+++ src/lib/categories.js
@@ -0,0 +1,25 @@
+export default () => {
+  return fetch('http://apps.wearetouro.com/api/torahSubcategories/catsSubcats%7CtouroMain')
+  .then((response) => {
+      return response.json();
+  })
+  .then((data) => {
+      //console.log(data);
+      let categoriesRepeated = [];
+      data.forEach(function(item){
+        categoriesRepeated.push(item.categoryValue);
+
+      });
+      var categories = categoriesRepeated.filter(function(item, index){
+        return categoriesRepeated.indexOf(item) >= index;
+      });
+      return categories;
+  });
+}
+
+
+
+
+
+
+
