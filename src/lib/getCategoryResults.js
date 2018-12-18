4c259c8580270c010d75478ee9e4b736128c8c19
Maya Davis <kryvitskaya@gmail.com>
2018-12-18 17:50:24 -0500

    fetching categories

--- src/lib/getCategoryResults.js
+++ src/lib/getCategoryResults.js
@@ -1,20 +1,22 @@
-export default function getCategoryResults(category){
-var http = new XMLHttpRequest();
-var url = "/api/esSetup.php";
-var params = {
-  default_operator: 'AND',
-  fieldquery: 'torahItem_categories',
-  from: 0,
-  index: 'torah-new',
-  size: 18,
-  'sort-by-direction': 'desc',
-  'sort-by': 'torahItem_recordingDate',
-  q: category
-}
-return 
-  http.open("GET", url+"?"+params, true);
-  http.onreadystatechange = function() {
-    console.log('test',http.responseText);
+import React from 'react';
+
+export default function getCategoryRsults(category){
+  var params = {
+    default_operator: 'AND',
+    fieldquery: 'torahItem_categories',
+    from: 0,
+    index: 'torah-new',
+    size: 18,
+    'sort-by-direction': 'desc',
+    'sort-by': 'torahItem_recordingDate',
+    q: category
   }
-  
+  return fetch (`/api/esSetup.php?${params}`)
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+ 
+
 }
+// https://stackoverflow.com/questions/35038857/setting-query-string-using-fetch-get-request
